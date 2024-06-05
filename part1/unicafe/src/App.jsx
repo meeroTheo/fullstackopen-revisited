5cca7cd932ca517f36793142f51e9778aa70e8e1
meeroTheo <meerbalochandhammy12@gmail.com>
2024-06-05 19:14:36 -0400

    1.7 -> add total state hook

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total,setTotal] = useState(0)
+  
 
 
   const Header = ({text}) => {
@@ -28,19 +30,26 @@ const App = () => {
     )
 
   }
+  const updateState = (setter,value) =>{
+    setter(value);
+    setTotal(total+1);
+  };
 
   return (
     <div>
       <Header text = {"give feedback"}/>
-      <Button handleClick={()=>setGood(good+1)} text="good"/>
-      <Button handleClick={()=>setNeutral(neutral+1)} text="neutral"/>
-      <Button handleClick={()=>setBad(bad+1)} text="bad"/>
+      <Button handleClick={()=>updateState(setGood,good+1)} text="good"/>
+      <Button handleClick={()=>updateState(setNeutral,neutral+1)} text="neutral"/>
+      <Button handleClick={()=>updateState(setBad,bad+1)} text="bad"/>
 
       <Header text = {"statistics"}/>
-
       <Display text={"good"} value = {good}/>
       <Display text={"neutral"} value = {neutral}/>
       <Display text={"bad"} value = {bad}/>
+      <Display text={"all"} value = {total}/>
+      <Display text={"average"} value = {(1*good + 0*neutral + -1*bad)/total}/>
+      <Display text={"positive"} value = {good/total}/>
+      
     </div>
   )
 };
